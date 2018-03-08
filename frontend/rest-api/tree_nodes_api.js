9ff8326c18785df2966c7f5ffef75f662f251405
Ryan Mease <rsmease@gmail.com>
2018-03-08 11:14:04 -0800

    Add tree node api methods

---
+++ frontend/rest-api/tree_nodes_api.js
@@ -0,0 +1,26 @@
+export const fetchAllNodes = () => (
+    $.ajax({
+        url: 'api/tree_nodes',
+        method: 'GET'
+    })
+);
+
+export const postTreeNode = (treeNode) => (
+    $.ajax({
+        url: 'api/tree_nodes',
+        method: 'POST',
+        data: {
+            tree_node: treeNode
+        }
+    })
+);
+
+export const patchTreeNode = (treeNode) => (
+    $.ajax({
+        url: `api/projects/${treeNode.id}`,
+        method: 'PATCH',
+        data: {
+            tree_node: treeNode
+        }
+    })
+);
